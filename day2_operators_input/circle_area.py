c76a3df7a5a6b1707f309417ea09828701bf57d1
Kartik Bissa <1754.kartik@gmail.com>
2026-02-10 19:45:09 +0530

    Add files via upload

---
+++ day2_operators_input/circle_area.py
@@ -0,0 +1,13 @@
+'Program to calculate area of circle'
+r = float(input("Enter radius Of Circle: "))
+area = 3.14 * r * r
+print("Area of circle is: ", area)
+
+'Program to swap two numbers'
+a = 34
+b = 45
+print(a)
+print(b)
+a, b = b, a
+print(a)
+print(b)
