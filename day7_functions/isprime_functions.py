c9338c279261f2dcb2b5b953f927acb06ac5d053
Kartik Bissa <1754.kartik@gmail.com>
2026-02-15 10:21:27 +0530

    Create isprime_functions.py

---
+++ day7_functions/isprime_functions.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+print(is_prime(2))   
+print(is_prime(15))  
+print(is_prime(17))  
